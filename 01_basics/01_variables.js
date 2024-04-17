21dda97e096bde347d286a06d15d84a7d1b20a6d
beingPro007 <120173992+beingPro007@users.noreply.github.com>
2024-04-17 10:36:27 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1444553
+let accountEmail = "gautamelon@gmail.com"
+var accountPwd = "12345"
+accountCity = "Jaipur"
+    // accountId = 2  -->not allowed
+accountEmail = "gr@gmail.com"
+accountPwd = "23222"
+accountstate = "Benguluru"
+
+let accountState;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+// treat all js codeas newer version
+// alert(3 + 3) we areusing nodejs not browser
+console.log(3 + 3)
+let name = "gautam"
+let age = 18
+let isLoggedIn = false
+console.log(typeof(age))
+console.log(typeof null)
